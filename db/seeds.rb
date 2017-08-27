52c2edab163e908bd12ee056c0f9be648a648524
Junta Ono <johnta_s@me.com>
2017-08-27 13:25:31 +0900

    seeds.rbにcsvからdbにデータを追加する処理を書いた

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+CSV.foreach('db/jta_free_wifi.csv') do |row|
+    Spot.create(:name => row[1], :english_name => row[2], :category => row[4], :prefecture => row[7], :address => row[8], :address_in_english => row[9], :ssid => row[12], :url => row[18], :longitude => row[19], :latitude => row[20])
+end
