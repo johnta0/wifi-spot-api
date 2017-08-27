c279147f2e9caa4b9204b317e108f91dacd2d06b
Junta Ono <johnta_s@me.com>
2017-08-27 14:06:02 +0900

    seedの作り直し

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
 require 'csv'
 
 CSV.foreach('db/jta_free_wifi.csv') do |row|
-    Spot.create(:name => row[1], :english_name => row[2], :category => row[4], :prefecture => row[7], :address => row[8], :address_in_english => row[9], :ssid => row[12], :url => row[18], :longitude => row[19], :latitude => row[20])
+
+    # データ挿入
+    Spot.create(:name => row[1], :english_name => row[2], :category => row[4], :prefecture => row[7], :address => row[8], :address_in_english => row[9], :ssid => row[12], :url => row[17], :longitude => row[18], :latitude => row[19])
+
 end
+
