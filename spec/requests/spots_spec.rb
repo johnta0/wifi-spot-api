31f174cd1fd3cfdfea15ad2c468e08e94605b6fd
Junta Ono <johnta_s@me.com>
2017-08-31 14:44:23 +0900

    update

--- spec/requests/spots_spec.rb
+++ spec/requests/spots_spec.rb
@@ -12,8 +12,10 @@ RSpec.describe "Spots", type: :request do
       expect(json.size).to eq 5
       (0..4).each do |i|
         expect(json[i]["name"]).to eq Spot.all[i].name
-        expect(json[i]["address"]).to eq Spot.all[i].name
-        # expect(json[i]["distance"]).to eq Spot.all[i].name
+        expect(json[i]["address"]).to eq Spot.all[i].address
+        # expect(json[i]["distance"]).to eq 
+      end
+
     end
   end
 end
