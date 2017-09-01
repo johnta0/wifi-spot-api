20b624a4bb4cd623a0f070257a5181dd6fbfc8df
Junta Ono <johnta_s@me.com>
2017-09-01 16:43:13 +0900

    /api/v1/spots?(params) のテスト

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,2 @@
-require 'csv'
-
-CSV.foreach('db/jta_free_wifi.csv') do |row|
-
-    # データ挿入
-    Spot.create(:name => row[1], :english_name => row[2], :category => row[4], :prefecture => row[7], :address => row[8], :address_in_english => row[9], :ssid => row[12], :url => row[17], :lat => row[18], :lng => row[19])
-
-end
+load(Rails.root.join( 'db', 'seeds', "#{Rails.env.downcase}.rb"))
 
--- spec/requests/spots_spec.rb
+++ spec/requests/spots_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Spots", type: :request do
-  describe "GET /api/v1/spots" do
 
-    before { @spot = FactoryGirl.create(:spot) }
+  describe "GET /api/v1/spots" do
 
     it "spots一覧の取得ができること" do
 
-      get api_v1_spots_path
+      get '/api/v1/spots'
 
       # status code の確認
       expect(response).to have_http_status(200)
@@ -15,24 +14,64 @@ RSpec.describe "Spots", type: :request do
       # json
       json = JSON.parse(response.body)
       expect(json.size).to eq 5
-      # check_json_contents(json, 4)
 
+    end
+
+  end
+
+  describe "GET /api/v1/spots?(パラメータ指定)" do
+
+    before do
+      @lat = 35.681167, @lng = 139.767052, @radius = 1000
+
+      get '/api/v1/spots', params: {radius: 1000, latitude: 35.681167, longitude: 139.767052, limit: 3}
+      @json_by_params = JSON.parse(response.body)
     end
 
     it "クエリパラメータを指定してレスポンスを得られること" do
 
-      get api_v1_sopts_path, params: {radius: 1000, latitude: 35.681167, longitude: 139.767052, limit: 10}
-
       # status code
-      expect(reponse).to have_http_status(200)
+      expect(response).to have_http_status(200)
 
-      # json
-      json = JSON.parse(response.body)
-      expect(json.size).to eq 5
-      # check_json_contents(9)
+    end
+
+    it "limit通りの数のjsonが返ってきてること" do
+     expect(@json_by_params.size).to eq 3
+    end
+
+    it "nameが正しいこと" do
+      expect(@json_by_params[1]["name"].to eq Spot.sort_by_distance(@lat, @lng, @radius).limit(3)[1]["name"])
+    end
+
+    it "addressが正しいこと" do
+      expect(@json_by_params[1]["address"].to eq Spot.sort_by_distance(@lat, @lng, @radius).limit(3)[1]["address"])
+    end
+
+    it "distanceが正しいこと" do
+      expect(@json_by_params[1]["distance"].to eq Spot.sort_by_distance(@lat, @lng, @radius).limit(3)[1].distance_from([@lat, @lng], :units=>:meters))
     end
 
   end
+
+  describe 'GET /api/v1/spots/:id' do
+
+    before do
+      get '/api/v1/spots/5'
+      @json_by_id = JSON.parse(response.body)
+    end
+
+    it "idで指定したスポットの情報を得られること" do
+      expect(response).to have_http_status(200)
+    end
+
+    it "idが正しいこと" do
+      expect(@json_by_id["id"]).to eq 5
+    end
+
+  end
+
+
+
 end
 
 
