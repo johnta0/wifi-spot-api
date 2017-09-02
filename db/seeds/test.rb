1c308ba3d9a2db7d229328f9902c22e588ff6492
Junta Ono <johnta_s@me.com>
2017-09-02 12:37:37 +0900

    production用のseedを別に

---
+++ db/seeds/test.rb
@@ -0,0 +1,9 @@
+require 'csv'
+
+CSV.foreach('db/test.csv') do |row|
+
+    # データ挿入
+    Spot.create(:name => row[1], :english_name => row[2], :category => row[4], :prefecture => row[7], :address => row[8], :address_in_english => row[9], :ssid => row[12], :url => row[17], :lat => row[18], :lng => row[19])
+
+end
+
